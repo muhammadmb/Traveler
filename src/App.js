2cf110ff8fefe9de7d3e3bf55554293419ef84e5
muhammadmb <muhammadmagdyje@gmail.com>
2021-03-15 06:24:10 +0200

    design responsive navigation bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter >
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </BrowserRouter>
 
     </div>
   );
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import './NavbarStyle.css';
+import { Link } from 'react-router-dom';
+import { Button } from '../Button/Butoon';
+
+function Navbar() {
+
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+    const handleClick = () => setClick(!click);
+    const closeMoblieMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton();
+    }, [])
+
+    window.addEventListener('resize', showButton);
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link
+                        to="/"
+                        className="navbar-logo"
+                        onClick={closeMoblieMenu}
+                    >
+                        TVL <i className="fab fa-typo3" />
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fa fa-times' : 'fa fa-bars'} />
+                    </div>
+
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link
+                                to="/home"
+                                className="nav-links"
+                                onClick={closeMoblieMenu}
+                            >
+                                Home
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link
+                                to="/services"
+                                className="nav-links"
+                                onClick={closeMoblieMenu}
+                            >
+                                Services
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link
+                                to="/products"
+                                className="nav-links"
+                                onClick={closeMoblieMenu}
+                            >
+                                Products
+                            </Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link
+                                to="/sign-up"
+                                className="nav-links-mobile"
+                                onClick={closeMoblieMenu}
+                            >
+                                Sign Up
+                            </Link>
+                        </li>
+
+                    </ul>
+
+                    {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
+
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
